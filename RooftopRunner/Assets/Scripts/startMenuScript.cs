3e53d3583fad4c8088f036b110ab58d0680c5b05
xiarui22 <you@example.com>
2017-04-17 22:00:40 -0400

    start menu added

---
+++ RooftopRunner/Assets/Scripts/startMenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class startMenuScript : MonoBehaviour {
+    public Canvas menu;
+    public Canvas cmenu;
+    public Button playButton;
+    public Button creditButton;
+    public Button closecreditButton;
+    // Use this for initialization
+    void Start () {
+        cmenu.enabled = false;
+
+    }
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Lv1-Peng");
+    }
+    public void CreditButton()
+    {
+        menu.enabled = !menu.enabled;
+        cmenu.enabled = !cmenu.enabled;
+    }
+    public void CloseCreditButton()
+    {
+        menu.enabled = !menu.enabled;
+        cmenu.enabled = !cmenu.enabled;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
